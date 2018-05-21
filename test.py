e9e1774dc2841c9fe9bfd718c6057798ca920e7d
Seanie Lee <lsnfamily02@naver.com>
2018-05-21 21:00:37 +0900

    edit train.py

---
+++ test.py
@@ -0,0 +1,67 @@
+from dense_bigru import DenseBiGRU
+import data_util
+import tensorflow as tf
+import os
+
+
+max_vocab_size = 15e4
+learning_rate = 0.1
+batch_size = 32
+dropout = 0.5
+embedding_size = 300
+num_layers = 1
+summary_len = 100
+attention_hidden_size = 100
+beam_depth = 5
+state_size = 120
+mode = "test"
+doc_file = "data/test_article.txt"
+sum_file = "data/test_abstract.txt"
+vocab_file = "data/vocab"
+checkpoint_dir = "./save/baseline/checkpoints"
+checkpoint_prefix = os.path.join(checkpoint_dir, "baseline")
+vocab = data_util.Vocab("data/vocab", max_vocab_size)
+docs = data_util.load_test_data(doc_file, vocab)
+summary_file = "result/summaries.txt"
+with tf.Graph().as_default():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = 0.9
+    sess = tf.Session()
+    log_writer = tf.summary.FileWriter(checkpoint_dir, graph=sess.graph)
+    model = DenseBiGRU(vocab_size=max_vocab_size, embedding_size=embedding_size,
+                       num_layers=num_layers, state_size=state_size,
+                       decoder_vocab_size=max_vocab_size,
+                       attention_hidden_size=attention_hidden_size, mode=mode,
+                       beam_depth=beam_depth, learning_rate=learning_rate,
+                       max_iter=summary_len)
+    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        print("Reload model parameters")
+        model.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        exit()
+    batches = data_util.batch_loader(docs, batch_size)
+    fw = open(summary_file, "w", encoding="utf-8")
+    for idx, batch in enumerate(batches):
+        batch_encoder_input, batch_encoder_len = data_util.make_array_format(
+            batch)
+        # if BeamSearchDecoder : [batch, max_time_step, beam_Depth]
+        batch_summaries = model.predict(sess, batch_encoder_input,
+                                        batch_encoder_len)
+        # iterate every example, get best beam search result
+        for summary in batch_summaries:
+            best_summary = summary[:, 0]
+            # convert indices to corresponding tokens
+            summary_tokens = data_util.map_idx2tok(best_summary, vocab)
+            # remove EOS token
+            try:
+                stop_index = summary_tokens.index(data_util.ID_EOS)
+                summary_tokens = summary_tokens[:stop_index]
+            except ValueError:
+                summary_tokens = summary_tokens
+            results = " ".join(summary_tokens)
+            fw.write(results + "\n")
+        print("decoding {}th batch from data".format(idx))
+    print("end of decoding")
+    fw.close()
--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 from dense_bigru import DenseBiGRU
-import data_util
+from data_util import batch_loader, load_data, load_valid_data, \
+    make_array_format
 import os
+import numpy as np
 
 max_vocab_size = 15e4
 learning_rate = 0.1
@@ -19,10 +21,12 @@ sum_file = "data/train_abstract.txt"
 vocab_file = "data/vocab"
 checkpoint_dir = "./save/baseline/checkpoints"
 checkpoint_prefix = os.path.join(checkpoint_dir, "baseline")
+dev_doc_file = "data/val_article.txt"
+dev_sum_file = "data/val_abstract.txt"
 # load source and target data
-docs, sums, vocab = \
-    data_util.load_data(doc_file, sum_file, vocab_file, max_vocab_size,
-                        debug=True)
+docs, sums, vocab = load_data(doc_file, sum_file, vocab_file, max_vocab_size,
+                              debug=True)
+dev_docs, dev_sums = load_valid_data(dev_doc_file, dev_sum_file, vocab)
 vocab_size = vocab.size()
 # self, vocab_size, embedding_size, state_size, num_layers,
 #                  decoder_vocab_size, attention_hidden_size, mode, beam_depth,
@@ -51,15 +55,17 @@ with tf.Graph().as_default():
         print("create new model parameter")
         sess.run(tf.global_variables_initializer())
     step = None
+    best_result = 1e10
     for epoch in range(num_epochs):
-        batches = data_util.batch_loader(list(zip(docs, sums)), batch_size)
-
+        batches = batch_loader(list(zip(docs, sums)), batch_size)
+        dev_batches = batch_loader(
+            list(zip(dev_doc_file, dev_sum_file)), batch_size)
         for batch in batches:
             batch_source, batch_target = zip(*batch)
-            batch_encoder_input, batch_encoder_len, batch_decoder_input, \
-            batch_decoder_len = data_util.make_array_format(batch_source,
-                                                            batch_target)
-            batch_loss, summary = model.train(sess,
+            batch_encoder_input, batch_encoder_len, \
+            batch_decoder_input, batch_decoder_len = \
+                make_array_format(batch_source, batch_target)
+            batch_loss, summary_op = model.train(sess,
                                               encoder_inputs=batch_encoder_input,
                                               encoder_length=batch_encoder_len,
                                               decoder_inputs=batch_decoder_input,
@@ -68,7 +74,20 @@ with tf.Graph().as_default():
             step = model.global_step.eval(sess)
             print("epoch : {} step : {}, loss : {}".format(epoch + 1, step,
                                                            batch_loss))
-
+        losses = []
+        for batch in dev_batches:
+            source, target = zip(*batch)
+            encoder_input, encoder_len, decoder_input, \
+            decoder_len = make_array_format(source, target)
+            model.mode = "test"
+            loss, summary_op = model.eval(sess, encoder_input, encoder_len,
+                                   decoder_input, decoder_len)
+            losses.append(loss)
+        result = np.mean(losses)
+        if result < best_result:
+            best_result = result
+            print("new record loss : {}".format(result))
             model.save(sess, checkpoint_prefix, step)
+        model.mode = "train"
 
     print("end of training")
